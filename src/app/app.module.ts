1e6acf12342a553e0b11a9e51dd6d7acb160517f
Vishek Kumar <vishek2@gmail.com>
2019-02-09 20:17:10 +0530

    Added Product Detail Blueprint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    ProductListComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
