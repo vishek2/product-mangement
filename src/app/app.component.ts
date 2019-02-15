36f2b70c16381b07a848964bab75dfeb10850603
Vishek Kumar <vishek2@gmail.com>
2019-02-16 00:45:01 +0530

    Added one way binding (property binding and event binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public pageTitle: string = 'My Product Management';
+  public pageTitle: string = 'Product Management';
 }
